5748d27d39f492b142619fd3085cfe0dcb8ef1b7
TeodoraLazaroiu <lazaroiuteodora@yahoo.com>
2022-04-10 14:17:38 +0300

    Update

--- Algoritmi Avansati/Laboratoare/Lab2/chromosome.py
+++ Algoritmi Avansati/Laboratoare/Lab2/chromosome.py
@@ -1,3 +1,5 @@
+import random
+
 class Chromosome:
     def __init__(self, a, b, n, bits, precision = 0):
         self.a = a
@@ -11,18 +13,59 @@ class Chromosome:
 
         n = int(string, 2)
         n = n * self.precision
-        self.a = self.a + n
+
+        return self.a + n
+
+def randomBits (n, length):
+    lista = []
+    for i in range(n):
+        bits = []
+        for i in range(length):
+            bits.append(random.randint(0,1))
+        lista.append(bits)
+    
+    return lista
 
 # a = 2, b = 3, n = 3, bits = [1,1,1], p = 0.125
 # transform: bits = 7, 0.125 * 7 = 0.875, a = 2 + 0.875 = 2.875
 c1 = Chromosome(2,3,3,[1,1,1])
-print(c1.a)
+# print(c1.a)
 c1.transform()
-print(c1.a)
+# print(c1.a)
 
 # a = 1, b = 4, n = 3, bits = [1,0,1], p = 0.375
 # transform: bits = 5, 0.375 * 5 = 1.875, a = 1 + 1.875 = 2.875
 c2 = Chromosome(1,4,3,[1,0,1])
-print(c2.a)
+# print(c2.a)
 c2.transform()
-print(c2.a)
+# print(c2.a)
+
+c3 = Chromosome(-1, 2,22, [0,0,0,0,0,1,1,1,0,1,0,0,1,0,0,1,1,1,0,0,0,1])
+# print(c3.transform())
+
+g = open("evolution.txt", "w")
+
+# 20 de cromozomi de lungime 22
+n = 20
+ch = randomBits(n, 22)
+
+# se creeaza lista de cromozomi cu
+# valorile random generate anterior
+
+chromosomes = []
+for i in range(n):
+    c = Chromosome(-1, 2, 22, ch[i])
+    chromosomes.append(c)
+
+x = [c.transform() for c in chromosomes]
+f = [(-1) * (x**2) + x  + 2 for x in x]
+
+g.write("Populatia initiala:\n")
+for i in range(n):
+    string = ""
+    bits = [str(x) for x in ch[i]]
+    bits = "".join(bits)
+    string = string + str(i) + ": " + bits + "\n"
+    g.write(string)
+
+g.close()
--- Inteligenta Artificiala/Laborator 5/lab5.py
+++ Inteligenta Artificiala/Laborator 5/lab5.py
@@ -14,3 +14,19 @@ w1 = np.random.randn()
 w2 = np.random.randn()
 b = np.random.randn()
 
+for epoch in range(20):
+    #calculate errors (using derivative formula E(w) - w0 / w - w0)
+    for sample,target in zip(x_train,y_train):
+        par = w1*sample[0]+w2*sample[1]+b-target
+        #calculate derivative dE/dw1 ((w1x1+w2x2+b-t)^2) = 2*(w1x1+w2x2+b-t)*(x1)
+        w1 = w1 - 2 * par * sample[0] * lr
+        w2 = w2 - 2 * par * sample[1] * lr
+        b = b - 2 * par * 1 * lr
+
+    print("w1,w2,b: ",w1,w2,b)
+
+    test_predictions = []
+    for test_sample in x_test:
+        test_predictions.append(w1*test_sample[0]+w2*test_sample[1]+b)
+    test_predictions=np.array(test_predictions)
+    print("mean:    ",np.mean(np.abs(test_predictions-y_test)))
