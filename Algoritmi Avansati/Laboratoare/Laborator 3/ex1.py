1945e4639296809dbb0be4c0af8a2659488ff795
TeodoraLazaroiu <lazaroiuteodora@yahoo.com>
2022-06-03 15:44:13 +0300

    Add files

--- Algoritmi Avansati/Laboratoare/Laborator 3/ex1.py
+++ Algoritmi Avansati/Laboratoare/Laborator 3/ex1.py
@@ -26,22 +26,84 @@ def randomBits (n, length):
     
     return lista
 
-# a = 2, b = 3, n = 3, bits = [1,1,1], p = 0.125
-# transform: bits = 7, 0.125 * 7 = 0.875, a = 2 + 0.875 = 2.875
-c1 = Chromosome(2,3,3,[1,1,1])
-# print(c1.a)
-c1.transform()
-# print(c1.a)
+#
+# functii pentru algoritmul de evolutie
+#
 
-# a = 1, b = 4, n = 3, bits = [1,0,1], p = 0.375
-# transform: bits = 5, 0.375 * 5 = 1.875, a = 1 + 1.875 = 2.875
-c2 = Chromosome(1,4,3,[1,0,1])
-# print(c2.a)
-c2.transform()
-# print(c2.a)
+def populatieInitiala():
+    g.write("Populatia initiala:\n")
+    for i in range(n):
+        string = ""
+        bits = [str(x) for x in ch[i]]
+        bits = "".join(bits)
+        string = string + str(i) + ": " + bits + " "
+        string = string + "x = " + str(x[i]) + " "
+        string = string + "f = " + str(f[i]) + "\n"
+        g.write(string)
+    g.write("\n")
 
-c3 = Chromosome(-1, 2,22, [0,0,0,0,0,1,1,1,0,1,0,0,1,0,0,1,1,1,0,0,0,1])
-# print(c3.transform())
+def probabilitateSelectie():
+    sumProb = sum(f)
+    probabilitati = []
+    g.write("Probabilitate selectie:\n")
+    for i in range(n):
+        string = "cromozom " + str(i) + ": probabilitate = "
+        p = f[i] / sumProb
+        probabilitati.append(p)
+        string = string + str(p) + "\n"
+        g.write(string)
+    g.write("\n")
+    probabilitati.sort()
+    return probabilitati
+
+def intervaleProbabilitati(p):
+    intervale = [0]
+    g.write("Intervale probabilitate selectie:\n")
+    g.write("0 ")
+    for i in range(len(p) - 1):
+        interv = intervale[-1] + p[i]
+        intervale.append(interv)
+        g.write(str(interv))
+        g.write(" ")
+    g.write("1")
+    g.write("\n")
+    return intervale
+
+def cautareBinara(intervale, left, right, x):
+    if right - left == 1:
+        return left
+    else:
+        mid = (left + right) // 2
+        if x > intervale[mid]:
+            return cautareBinara(intervale, mid, right, x)
+        else:
+            return cautareBinara(intervale, left, mid, x)
+
+def selectie(intervale, populatieVeche):
+    cromozomiSelectati = []
+    for i in range(n):
+        u = random.random()
+        cromozom = cautareBinara(intervale, 0, len(intervale), u)
+        cromozomiSelectati.append(cromozom)
+        g.write(f'\nu = {u}, selectam cromozomul {cromozom}')
+    g.write("\n")
+
+    g.write('\nPopulatia dupa selectie:\n')
+    populatieNou = [0] * n
+    for i, cromozom in enumerate(cromozomiSelectati):
+        cromozomNou = populatieVeche[cromozom - 1]
+        populatieNou[i] = cromozomNou
+        xNou = cromozomNou.transform()
+        fNou = f[cromozom - 1]
+        bits = [str(x) for x in cromozomNou.bits]
+        bits = "".join(bits)
+        g.write(f'{i+1}: {bits} x = {xNou} f = {fNou}\n')
+        
+    return populatieNou
+
+#
+# main entry
+#
 
 g = open("evolution.txt", "w")
 
@@ -52,20 +114,17 @@ ch = randomBits(n, 22)
 # se creeaza lista de cromozomi cu
 # valorile random generate anterior
 
-chromosomes = []
+cromozomi = []
 for i in range(n):
     c = Chromosome(-1, 2, 22, ch[i])
-    chromosomes.append(c)
+    cromozomi.append(c)
 
-x = [c.transform() for c in chromosomes]
+x = [c.transform() for c in cromozomi]
 f = [(-1) * (x**2) + x  + 2 for x in x]
 
-g.write("Populatia initiala:\n")
-for i in range(n):
-    string = ""
-    bits = [str(x) for x in ch[i]]
-    bits = "".join(bits)
-    string = string + str(i) + ": " + bits + "\n"
-    g.write(string)
+populatieInitiala()
+probabilitati = probabilitateSelectie()
+intervale = intervaleProbabilitati(probabilitati)
+cromozomi = selectie(intervale, cromozomi)
 
 g.close()
