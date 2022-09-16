e203e202edfc26237d8a6482fd9f9661bfb19f39
TeodoraLazaroiu <lazaroiuteodora@yahoo.com>
2022-09-16 21:38:43 +0300

    Add files

--- Fundamentele Limbajelor de Programare/241_Lazaroiu_Teodora_Bianca_Var1_P1.pl
+++
@@ -1,7 +0,0 @@
-cate_rasp_corecte([],[], _, 0).
-cate_rasp_corecte([H1|T1], [H2|T2], Nume, N) :- cate_rasp_corecte(T1, T2, Nume, Numar), ((H1 == H2) -> N is Numar+1; N is Numar).
-
-/**
-? - cate_rasp_corecte([a,b,c,d,a],[a,a,a,a,a],Teodora,N).
-N = 2.
-*/
--- Fundamentele Limbajelor de Programare/labs+sem/labs+sem/labs/lab1/kb1.pl
+++
@@ -1,7 +0,0 @@
-bigger(elephant, horse).
-bigger(horse, donkey).
-bigger(donkey, dog).
-bigger(donkey, monkey).
-
-is_bigger(X, Y) :- bigger(X, Y).
-is_bigger(X, Y) :- bigger(X, Z), is_bigger(Z, Y).
